acbe3d7aa5b2e1a8b18f42fd04babe2dc9beeb84
Val Parajara <valeriaparajara@gmail.com>
2018-01-25 12:51:59 -0200

    Initial commit

---
+++ app/loans.js
@@ -0,0 +1,1 @@
+console.log("Lista de Emprestimos");
---
+++ server.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var express = require('express')
+ , app = express();
+ app.get('/', function(req, res){
+   res.send('Hello World!');
+});
+app.listen(3000);
