e29d06f67be0473cd52b5a20bde972236acbf421
Valéria Parajara <valeriaparajara@gmail.com>
2018-01-30 17:36:02 -0200

    Create files controllers users and loans

---
+++ app/controllers/loan.js
@@ -0,0 +1,21 @@
+var Loan = require('../models/loan.js');
+
+exports.create = function(req, res) {
+
+};
+
+exports.findAll = function(req, res) {
+
+};
+
+exports.findOne = function(req, res) {
+
+};
+
+exports.update = function(req, res) {
+
+};
+
+exports.delete = function(req, res) {
+
+};
---
+++ app/controllers/user.js
@@ -0,0 +1,13 @@
+var User = require('../models/user.js');
+
+exports.create = function(req, res) {
+
+};
+
+exports.update = function(req, res) {
+
+};
+
+exports.delete = function(req, res) {
+
+};
